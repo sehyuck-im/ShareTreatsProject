6aade0e1d7f288b3a1d2deb246fca49b14eb58f0
sehyuck <imse2591@gmail.com>
2023-04-18 19:35:51 +0900

    Create ExchangeServiceImplTest.java

---
+++ src/test/java/com/ShareTreats/ShareTreatsProject/ExchangeGoods/Service/ExchangeServiceImplTest.java
@@ -0,0 +1,18 @@
+package com.ShareTreats.ShareTreatsProject.ExchangeGoods.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ExchangeServiceImplTest {
+
+    @Test
+    void ready10GoodsCode(){ //상품 코드는 10개가 준비되면 고객에게 10개까지만 제공됩니다.
+
+
+
+
+    }
+}
