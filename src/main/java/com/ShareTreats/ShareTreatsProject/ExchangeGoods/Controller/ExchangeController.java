dfbe78ddf86207315f6e2eed4a7d292b9b2621d0
sehyuck <imse2591@gmail.com>
2023-04-18 17:42:55 +0900

    Create ExchangeController.java

---
+++ src/main/java/com/ShareTreats/ShareTreatsProject/ExchangeGoods/Controller/ExchangeController.java
@@ -0,0 +1,9 @@
+package com.ShareTreats.ShareTreatsProject.ExchangeGoods.Controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ExchangeController {
+    
+
+}
