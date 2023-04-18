2b933771dbd0087731c7ffff6411494e1fa1950d
sehyuck <imse2591@gmail.com>
2023-04-18 17:42:51 +0900

    add Model for ExchangeGoods

--- src/main/java/com/ShareTreats/ShareTreatsProject/ExchangeGoods/Model/GoodsCode.java
+++ src/main/java/com/ShareTreats/ShareTreatsProject/ExchangeGoods/Model/GoodsCode.java
@@ -4,7 +4,11 @@ import lombok.Data;
 
 @Data
 public class GoodsCode {
+    // 상품 코드는 0~9 자연수 글자로 이루어져 있으며 9문자로 이루어져 있습니다.
     private String code;
-    private boolean isChanged = false;
+    // 상품을 교환 했는지 확인
+    private boolean isExchanged;
+    // 상품 교환한 상점
+    private Store exchangedStore;
 
 }
--- src/main/java/com/ShareTreats/ShareTreatsProject/ExchangeGoods/Model/Store.java
+++ src/main/java/com/ShareTreats/ShareTreatsProject/ExchangeGoods/Model/Store.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class Store {
+    // 상점 코드는 A~Z,a~z 까지의 대,소 영문자만 사용이 가능하며 6문자로 이루어져 있습니다.
     private String storeCode;
 
 }
