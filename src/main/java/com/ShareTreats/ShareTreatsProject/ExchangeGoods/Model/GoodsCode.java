2f7c393df482a97f64d395b43357ec00d2ec74e9
sehyuck <imse2591@gmail.com>
2023-04-18 20:32:04 +0900

    change Junit5 to Junit4

--- src/main/java/com/ShareTreats/ShareTreatsProject/ExchangeGoods/Model/GoodsCode.java
+++ src/main/java/com/ShareTreats/ShareTreatsProject/ExchangeGoods/Model/GoodsCode.java
@@ -2,6 +2,8 @@ package com.ShareTreats.ShareTreatsProject.ExchangeGoods.Model;
 
 import lombok.Data;
 
+import java.util.Random;
+
 @Data
 public class GoodsCode {
     // 상품 코드는 0~9 자연수 글자로 이루어져 있으며 9문자로 이루어져 있습니다.
@@ -10,5 +12,8 @@ public class GoodsCode {
     private boolean isExchanged;
     // 상품 교환한 상점
     private Store exchangedStore;
+    // 코드는 9자리
+    private static final int CODE_LENGTH = 9;
+    private static final Random RANDOM = new Random();
 
 }
--- src/test/java/com/ShareTreats/ShareTreatsProject/ExchangeGoods/Service/ExchangeServiceImplTest.java
+++ src/test/java/com/ShareTreats/ShareTreatsProject/ExchangeGoods/Service/ExchangeServiceImplTest.java
@@ -1,7 +1,12 @@
 package com.ShareTreats.ShareTreatsProject.ExchangeGoods.Service;
 
+import com.ShareTreats.ShareTreatsProject.ExchangeGoods.Model.GoodsCode;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,10 +14,28 @@ import static org.junit.jupiter.api.Assertions.*;
 class ExchangeServiceImplTest {
 
     @Test
-    void ready10GoodsCode(){ //상품 코드는 10개가 준비되면 고객에게 10개까지만 제공됩니다.
+    void ready10GoodsCodes(){ //상품 코드는 10개가 준비되면 고객에게 10개까지만 제공됩니다.
 
+        List<GoodsCode> goodsCodeList = new ArrayList<>();
+        for(int i=0; i<10; i++){
+            GoodsCode goodsCode = new GoodsCode();
 
+            System.out.println(i+"번 째");
+        }
+    }
 
+    private static final int CODE_LENGTH = 9;
+    private static final Random RANDOM = new Random();
 
+    @Test
+    String generateCode(){
+        // 1. 상품 코드는 0~9 자연수 글자로 이루어져 있으며 9문자로 이루어져 있습니다.
+        //      -> Random()을 사용해서 9자리 생성
+        StringBuilder sb = new StringBuilder(CODE_LENGTH);
+        for (int i = 0; i < CODE_LENGTH; i++) {
+            sb.append(RANDOM.nextInt(10));
+        }
+        System.out.println("sb = " + sb);
+        return sb.toString();
     }
 }
