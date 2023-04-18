c6bfd2b5182edf3e43a712eb373bf6a5a1e23403
sehyuck <imse2591@gmail.com>
2023-04-18 17:54:20 +0900

    add exchangeController Test

--- src/main/java/com/ShareTreats/ShareTreatsProject/ExchangeGoods/Controller/ExchangeController.java
+++ src/main/java/com/ShareTreats/ShareTreatsProject/ExchangeGoods/Controller/ExchangeController.java
@@ -1,9 +1,17 @@
 package com.ShareTreats.ShareTreatsProject.ExchangeGoods.Controller;
 
+import com.ShareTreats.ShareTreatsProject.ExchangeGoods.Service.ExchangeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
+@RequestMapping("/exchangeGoods")
 public class ExchangeController {
+
+    ExchangeService exchangeService;
+
+
 
 
 }
---
+++ src/test/java/com/ShareTreats/ShareTreatsProject/ExchangeGoods/Controller/ExchangeControllerTest.java
@@ -0,0 +1,15 @@
+package com.ShareTreats.ShareTreatsProject.ExchangeGoods.Controller;
+
+import com.ShareTreats.ShareTreatsProject.ExchangeGoods.Service.ExchangeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ExchangeControllerTest {
+    @Autowired
+    ExchangeService exchangeService;
+
+
+}
